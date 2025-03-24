a29f7cbddf0abf123d4348d38d1d707e1fc8cb0f
saebyeokjang <dev.saebyeok@gmail.com>
2025-03-24 15:55:41 +0900

    개발자 정보 추가

--- LOACheck/SettingsView.swift
+++ LOACheck/SettingsView.swift
@@ -88,6 +88,9 @@ struct SettingsView: View {
                 
                 Section(header: Text("만든 사람")) {
                     Link("개발자에게 피드백 보내기", destination: URL(string: "mailto:dev.saebyeok@gmail.com?subject=LOACheck 피드백")!)
+                    Text("실리안 • 기상술사김새벽")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
                 }
             }
             .navigationTitle("설정")
